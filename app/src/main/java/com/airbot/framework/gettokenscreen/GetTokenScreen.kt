47085f7dfdee5db22dc00647080009361aeb532e
Joseph Escribano <anotherline.studio@gmail.com>
2023-03-18 15:59:23 +0100

    Cambiar versiones a unas mas antiguas por conflictos,hacer la estructura del proyecto y guardar el token, 18/03/2023

--- app/src/main/java/com/airbot/framework/gettokenscreen/GetTokenScreen.kt
+++ app/src/main/java/com/airbot/framework/gettokenscreen/GetTokenScreen.kt
@@ -2,9 +2,8 @@ package com.airbot.framework.gettokenscreen
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -12,10 +11,13 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.airbot.utils.UiEvent
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -43,12 +45,10 @@ fun GetTokenScreen(
         }
     }
 
-    LaunchedEffect(key1 = true) {
-        viewModel.handleEvent(GetTokenContract.Event.insertToken)
-
-    }
 
     Scaffold(scaffoldState = scaffoldState) {
+
+
         Box(
             modifier = Modifier
                 .fillMaxWidth()
@@ -56,17 +56,51 @@ fun GetTokenScreen(
                 .background(color = MaterialTheme.colors.primary),
             contentAlignment = Alignment.Center
         ) {
-            OutlinedTextField(
-                shape = RoundedCornerShape(20.dp),
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    backgroundColor = White,
-                ),
-                placeholder = { Text(text = "Inserta el token") },
-                value = viewModel.token,
-                onValueChange = {
-                    viewModel.handleEvent(GetTokenContract.Event.onTokenChange(it))
-                })
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                OutlinedTextField(
+                    shape = RoundedCornerShape(20.dp),
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        backgroundColor = White,
+                    ),
+                    placeholder = { Text(text = "Inserta el token") },
+                    value = viewModel.token,
+                    onValueChange = {
+                        viewModel.handleEvent(GetTokenContract.Event.onTokenChange(it))
+                    })
+
+                Spacer(modifier = Modifier.size(30.dp))
+
+                OutlinedButton(
+                    modifier = Modifier
+                        .width(135.dp)
+                        .size(50.dp)
+                        .border(width = 1.dp, color = White, shape = RoundedCornerShape(8.dp)),
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 6.dp,
+                        pressedElevation = 8.dp,
+                        disabledElevation = 0.dp
+                    ),
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        backgroundColor = Blue
+                    ),
+                    onClick = {
+                        viewModel.handleEvent(GetTokenContract.Event.insertToken)
+                        scope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar("Token insertado ")
+                        }
+                    },
+                    shape = RoundedCornerShape(8.dp)
+                ) {
+                    Text(text = "Registrarse", color = White)
+                }
+            }
         }
+
     }
 
 }
